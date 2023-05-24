f60b67338ad5ffb40bff9f1f7138797fb7e51bd4
TobySanders <tobysanders18@gmail.com>
2023-05-24 10:26:33 +0100

    move table folder and rename init

--- src/Database/tables/meal_plans.sql
+++ src/Database/tables/meal_plans.sql
@@ -1,11 +1,11 @@
 -- create table meal_plans
 CREATE TABLE IF NOT EXISTS meal_plans(
-    meal_plan_id UUID PRIMARY KEY
+    meal_plan_id bigserial PRIMARY KEY
 );
 
 -- create table meal_plan_recipes
 CREATE TABLE IF NOT EXISTS meal_plan_recipes(
-    meal_plan_id UUID NOT NULL,
+    meal_plan_id bigint NOT NULL,
     recipe_id bigint NOT NULL,
     PRIMARY KEY (meal_plan_id, recipe_id),
     FOREIGN KEY (meal_plan_id) REFERENCES meal_plans(meal_plan_id),
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS meal_plan_recipes(
 );
 
 -- create function to find recipes in a meal plan
-CREATE OR REPLACE FUNCTION get_meal_plan_recipes(meal_plan_id UUID)
+CREATE OR REPLACE FUNCTION get_meal_plan_recipes(meal_plan_id bigint)
 RETURNS TABLE (recipe_id bigint) AS $$
 BEGIN
     RETURN QUERY
